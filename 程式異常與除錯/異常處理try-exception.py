cde81d7f74aaa82f06847732ba8bda5cd33daaa2
Jefferson2666 <91866895+Jefferson2666@users.noreply.github.com>
2023-01-17 20:56:16 +0800

    Update 異常處理try-exception.py

--- 程式異常與除錯/異常處理try-exception.py
+++ 程式異常與除錯/異常處理try-exception.py
@@ -2,7 +2,7 @@ def division(x,y):
     try:
         return x/y #try - except 指令
     except ZeroDivisionError: #除數為0時執行
-        print("除數不可為0")
+        print("除數不可為0") #a
 
 print(division(10,2))
 print(division(3,0))
